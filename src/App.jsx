610c1e6c1e0b7f29139f5b7c7c4509b9345fcffb
mobin <nicokar.mobin@gmail.com>
2024-11-26 08:45:05 -0800

    added logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,31 @@ import React, { useContext } from 'react';
 import "./App.css";
 import AuthContext from './context/AuthContext';
 import LoginPage from "./pages/LoginPage";
+import { signOut } from 'firebase/auth'; 
+import { auth } from './firebase'; // Import your Firebase auth instance
 
 function App() {
-  const { user } = useContext(AuthContext);
+  const { user, setUser } = useContext(AuthContext);
 
-
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+      console.log('User signed out');
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
+  console.log(user);
+  console.log(auth);
+  
   return (
     <div>
       {user ? (
         // User is signed in
         <div>
           <p>Welcome, {user.displayName}!</p>
+          <button onClick={handleSignOut}>Sign Out</button> 
           {/* ... other components for authenticated users ... */}
         </div>
       ) : (
